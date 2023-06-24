ad99724ac10baa864aa409233157bdb1f1174cba
hbb <hbb@hbbtekademy.com>
2023-06-24 19:31:51 +0530

    handle cmd line args and file size

--- cmd/main.go
+++ cmd/main.go
@@ -2,64 +2,68 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
-	"strings"
 	"time"
 )
 
-type args struct {
-	bufferLines  int
-	maxSizeBytes int
-	logFilename  string
+type logSink struct {
+	maxSizeBytes int64
+	logFile      string
+	bufSize      int64
 }
 
 func main() {
-	args := parseCmdlineArgs()
+	logSink := parseCmdlineArgs()
 
-	f, err := os.OpenFile(args.logFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(logSink.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Printf("Failed opening Log File: %s. Error: %v\n", args.logFilename, err)
+		log.Fatalf("failed opening log file: %s. error: %v", logSink.logFile, err)
 	}
 	defer f.Close()
 
+	fStat, err := f.Stat()
+	if err != nil {
+		log.Fatalf("failed getting log file stat. error: %v", err)
+	}
+	fsize := fStat.Size()
+
 	reader := bufio.NewReader(os.Stdin)
-	buf := make([]byte, 4*1024)
-	c := 0
+	buf := make([]byte, logSink.bufSize)
 	for {
 		n, err := reader.Read(buf)
 		if err != nil {
 			if err == io.EOF {
-				fmt.Println("break")
 				break
 			} else {
 				fmt.Printf("failed reading buffer. error: %v\n", err)
 				continue
 			}
 		}
-		c += n
+		fsize += int64(n)
 		f.Write(buf[:n])
-		if c >= args.maxSizeBytes {
-			c = 0
+		if fsize >= logSink.maxSizeBytes {
+			fsize = 0
 			f.Close()
-			os.Rename(args.logFilename, fmt.Sprintf("%s.%d", args.logFilename, time.Now().UnixMilli()))
-			f, err = os.OpenFile(args.logFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			os.Rename(logSink.logFile, fmt.Sprintf("%s.%d", logSink.logFile, time.Now().UnixMilli()))
+			f, err = os.OpenFile(logSink.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
-				fmt.Printf("Failed opening Log File: %s. Error: %v\n", args.logFilename, err)
+				fmt.Printf("failed opening log file: %s. error: %v\n", logSink.logFile, err)
 			}
 		}
 
 	}
 }
 
-func parseCmdlineArgs() *args {
-	home := os.Getenv("HOME")
-	if strings.TrimSpace(home) == "" {
-		home = "."
-	}
+func parseCmdlineArgs() *logSink {
+	logFile := flag.String("logfile", fmt.Sprintf("./%d.log", os.Getpid()), "full log file path including filename")
+	maxSizeBytes := flag.Int64("size", 10*1024*1024, "max log file size in bytes")
+	bufSize := flag.Int64("bufsize", 2*1024, "max read buffer size")
 
-	args := &args{bufferLines: 10000, maxSizeBytes: 100 * 1024 * 1024, logFilename: fmt.Sprintf("%s\\current.log", home)}
+	flag.Parse()
 
-	return args
+	return &logSink{maxSizeBytes: *maxSizeBytes, logFile: *logFile, bufSize: *bufSize}
 }
