eee1ed7818421b5754e72cf5a36ad58459a5ef43
HBB <h.bhate1805@gmail.com>
2024-07-14 11:21:03 +0530

    add max log lines and logfile perm params

--- log-sink.go
+++ log-sink.go
@@ -5,24 +5,27 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 )
 
-type logSink struct {
-	maxSizeMB    int64
-	maxSizeBytes int64
+type logSinkParams struct {
 	logFile      string
+	logFilePerm  fs.FileMode
+	maxLines     int64
+	maxSizeBytes int64
 	bufSize      int64
 }
 
 func main() {
-	logSink := parseCmdlineArgs()
+	params := parseCmdlineArgs()
 
-	f, err := os.OpenFile(logSink.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(params.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, params.logFilePerm)
 	if err != nil {
-		log.Fatalf("failed opening log file: %s. error: %v", logSink.logFile, err)
+		log.Fatalf("failed opening log file: %s. error: %v", params.logFile, err)
 	}
 	defer f.Close()
 
@@ -30,41 +33,72 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed getting log file stat. error: %v", err)
 	}
+
 	fsize := fStat.Size()
+	lines := int64(0)
 
 	reader := bufio.NewReader(os.Stdin)
-	buf := make([]byte, logSink.bufSize)
+	buf := make([]byte, params.bufSize)
 	for {
 		n, err := reader.Read(buf)
 		if err != nil {
 			if err == io.EOF {
 				break
-			} else {
-				fmt.Printf("failed reading buffer. error: %v\n", err)
-				continue
 			}
-		}
-		fsize += int64(n)
-		f.Write(buf[:n])
-		if fsize >= logSink.maxSizeBytes {
-			fsize = 0
-			f.Close()
-			os.Rename(logSink.logFile, fmt.Sprintf("%s.%d", logSink.logFile, time.Now().UnixMilli()))
-			f, err = os.OpenFile(logSink.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Printf("failed opening log file: %s. error: %v\n", logSink.logFile, err)
-			}
+			log.Printf("failed reading buffer. error: %v", err)
+			continue
 		}
 
+		_, err = f.Write(buf[:n])
+		if err != nil {
+			log.Printf("failed writing buffer to file. error: %v", err)
+			continue
+		}
+		fsize += int64(n)
+		lines++
+
+		if fsize >= params.maxSizeBytes || (params.maxLines > 0 && lines >= params.maxLines) {
+			fsize = 0
+			lines = 0
+
+			err := f.Close()
+			if err != nil {
+				log.Printf("failed closing %s. error: %v", f.Name(), err)
+				break
+			}
+			err = os.Rename(params.logFile, fmt.Sprintf("%s.%d", params.logFile, time.Now().UnixMilli()))
+			if err != nil {
+				log.Printf("failed renaming %s. error: %v", params.logFile, err)
+				break
+			}
+
+			f, err = os.OpenFile(params.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, params.logFilePerm)
+			if err != nil {
+				log.Printf("failed opening log file: %s. error: %v", params.logFile, err)
+				break
+			}
+		}
 	}
 }
 
-func parseCmdlineArgs() *logSink {
-	logFile := flag.String("logfile", fmt.Sprintf("./pid-%d.log", os.Getpid()), "full log file path including filename")
-	maxSizeMB := flag.Int64("size", 10, "max log file size in MB")
-	bufSize := flag.Int64("bufsize", 4*1024, "max read buffer size")
+func parseCmdlineArgs() *logSinkParams {
+	logFile := flag.String("logfile", fmt.Sprintf("./pid-%d.log", os.Getpid()), "Full log file path including filename.")
+	logFilePerm := flag.Uint("logfile-perm", 0, "Log file permission. (default 0640)")
+	maxSizeMB := flag.Int64("max-size", 10, "Max log file size in MB. If both max-size and max-lines is specified, logfile will rollover when either thresold is reached.")
+	maxLines := flag.Int64("max-lines", 0, "Max log file lines.")
+	bufSize := flag.Int64("buf-size", 4*1024, "Max read buffer size.")
 
 	flag.Parse()
 
-	return &logSink{maxSizeMB: *maxSizeMB, maxSizeBytes: *maxSizeMB * 1024 * 1024, logFile: *logFile, bufSize: *bufSize}
+	if *logFilePerm == 0 {
+		*logFilePerm = 0640
+	}
+
+	return &logSinkParams{
+		logFile:      filepath.Clean(*logFile),
+		logFilePerm:  fs.FileMode(*logFilePerm),
+		maxSizeBytes: *maxSizeMB * 1024 * 1024,
+		maxLines:     *maxLines,
+		bufSize:      *bufSize,
+	}
 }
